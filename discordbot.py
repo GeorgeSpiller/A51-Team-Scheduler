4a131c3acd44317e5ca22ef14cbd263f0041010e
GeorgeSpiller <57860253+GeorgeSpiller@users.noreply.github.com>
2022-07-31 13:18:00 +0100

    scheduler and discord bot added

---
+++ discordbot.py
@@ -0,0 +1,199 @@
+import json
+import re
+from discord.ext import commands
+from datetime import timedelta, date, datetime
+from main import PREVIOUS_SCHEDULES_TEXTFILE, get_teams, get_days_scrimming_teams, main
+
+# firstly, load data
+TEAM_SCHEDULE_INFO_JSONFILE = 'D:\\Users\\geosp\\Documents\\Code\\PY\\Projects\\A51 Team Scheduler\\ScheduleInfo.json'
+with open(TEAM_SCHEDULE_INFO_JSONFILE, 'r') as f:
+    data = json.load(f)   
+
+DISCORD_BOT_TOKEN_TEXTFILE = 'D:\\Users\\geosp\\Documents\\Code\\PY\\Projects\\A51 Team Scheduler\\diecordbottoken.txt'
+with open(DISCORD_BOT_TOKEN_TEXTFILE, 'r') as f:
+    token = f.readline() 
+
+LAST_UPDATED_DATE = datetime.strptime(data['Date Compiled'], "%Y-%m-%d %H:%M:%S.%f")
+LAST_UPDATED_STRING = f'{LAST_UPDATED_DATE.day}/{LAST_UPDATED_DATE.month} at {LAST_UPDATED_DATE.hour}:{LAST_UPDATED_DATE.minute}'
+
+ALLOWED_SERVERS = [714774324174782534, 743968283006337116, 999094760750981221]
+SCHEDULE_CHANNEL_ID = 816724490674634772 # Testing Server: 1001559765215883344 A51: 816724490674634772
+BOTCOMMANDS_ALLOWED_CHANNELS = [1001915039386701965, 714774324174782537] #A51BOTTEST, TESTSERVERGENERAL
+PROTECTED_COMMANDS_ALLOWED_ROLES = ['manager', 'Team Manager']
+
+PREVIOUS_SCHEDULE_STRING = ''
+PREVIOUS_SCHEDULE_LOOKBACK = 2
+
+client = commands.Bot(command_prefix='!')
+
+
+def replace_roleid_with_rolename(message, guild):
+    '''replaces any instances of role ID tags with the role corrisponding role's name.
+        Also removes any roles that are between ~~strikethrough~~ tags'''
+    # fist, get a list of all the ID's in the message
+    rids = re.findall('<@&\d{18}>', message)
+    rids = [i.replace('<@&', '').replace('>', '') for i in rids]
+
+
+    # Get each IDs name, and replace
+    ret = message
+    for rid in rids:
+        role = guild.get_role(int(rid))
+        if role != None:
+            rid_name = role.name.replace(' ', '').upper()
+            ret = ret.replace(rid, rid_name)
+    return ret
+
+
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
+    print(f'{client.user} is connected to the following servers:\n')
+    for guild in client.guilds:
+        for textChannel in guild.text_channels:
+            if textChannel.id == SCHEDULE_CHANNEL_ID:
+                messageList = []
+                async for message in textChannel.history(limit=PREVIOUS_SCHEDULE_LOOKBACK):
+                        messageList.append(message.content)
+                        PREVIOUS_SCHEDULE_STRING = '-----------------------------------------'.join(messageList)
+                        PREVIOUS_SCHEDULE_STRING = replace_roleid_with_rolename(PREVIOUS_SCHEDULE_STRING, guild)
+                        with open(PREVIOUS_SCHEDULES_TEXTFILE, 'w') as f:
+                            f.write(PREVIOUS_SCHEDULE_STRING)
+                break
+        print(f'{guild.name} ({guild.id})')
+        if guild.id not in ALLOWED_SERVERS:
+            print(f'leaving server: {guild.name} ({guild.id})')
+            await client.get_guild(int(guild.id)).leave()
+
+
+@client.command(
+    # ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+	help="Gives a list of all the teams that are currently affiliated with Arena 51!",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="[!ft] Gives a list of all A51 teams.",
+    aliases=['ft', 'fulteams', 'fullteam', 'fulteam', 'fteams', 'fteam']
+)
+async def fullteams(ctx):
+    print(f'{ctx.author.name} sent command !fullteams')
+    if ctx.channel.id in BOTCOMMANDS_ALLOWED_CHANNELS:
+	    await ctx.channel.send(f"The full team list is bellow, last updated on ``{LAST_UPDATED_STRING}``:\n```{', '.join(data['Teams'])}```")
+
+@client.command(
+    # ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+	help="Lists all the Arena 51 teams that are currently on the stream schedule, and therefore all the teams that could potentially be streamed. If you believe a team should be on here, please message spag!",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="[!at] Lists all teams that are currently on the stream schedule.",
+    aliases=['at', 'ateam', 'ateams', 'activet', 'activeteam', 'acteams']
+)
+async def activeteams(ctx):
+    print(f'{ctx.author.name} sent command !activeteams')
+    if ctx.channel.id in BOTCOMMANDS_ALLOWED_CHANNELS:
+	    await ctx.channel.send(f"The full list of teams that are currently on the stream schedule is bellow, last updated on ``{LAST_UPDATED_STRING}``:\n```{', '.join(data['Teams On Schedule'])}```")
+    
+@client.command(
+    # ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+	help="Gives the stream priority for a team, for example: !teamscore platpack . The score is arbitary, that is it's not based on anything such as days, but rather is a guideline as to how likely you will beon the next schedule.",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="[!tp] Gives the stream priority for a team.",
+    aliases=['tp', 'tpriority', 'teamp', 'teampri', 'teampriorities']
+)
+async def teampriority(ctx, arg):
+    print(f'{ctx.author.name} sent command !teampriority {arg}')
+    if ctx.channel.id in BOTCOMMANDS_ALLOWED_CHANNELS:
+        userInputTeamName = arg.upper().strip()
+        if userInputTeamName in data['Teams']:
+            if userInputTeamName in data['Teams On Schedule']:
+                for team in data['TeamScore']:
+                    if team[0] == userInputTeamName:
+                        teams_in_future_schedule = [t[0] for t in data["Future Schedule"]]
+                        is_on_future_schedule = "likely" if team[0] in teams_in_future_schedule else "un-likley"
+                        await ctx.channel.send(f"The team ``{userInputTeamName}`` has a stream priority of {team[1]}/{data['Highest Priority']}. It is currently {is_on_future_schedule} to be streamed next week. Last updated on ``{LAST_UPDATED_STRING}``")
+                        break
+            else:
+                await ctx.channel.send(f"The team ``{userInputTeamName}`` is not currently on the stream schedule. Last updated on ``{LAST_UPDATED_STRING}``")
+        else:
+            await ctx.channel.send(f"My bad chief, i cannot find ``{userInputTeamName}`` as a team, try !teams for a list of teams we have. Last updated on ``{LAST_UPDATED_STRING}``")
+
+@client.command(
+    # ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+	help="Lists all the teams that scrim on a particular day. For example: !whoscrimson monday",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="[!wso] Lists all the teams that scrim on a particular day.",
+    aliases=['wso', 'whoso', 'whoscrimo', 'whoscrimso', 'wscrimo']
+)
+async def whoscrimson(ctx, arg):
+    print(f'{ctx.author.name} sent command !whoscrimson {arg}')
+    if ctx.channel.id in BOTCOMMANDS_ALLOWED_CHANNELS:
+        processedInput = arg[:2].lower()
+        #   m, tu, w, th, f, sa, su,
+        #   0  1   2  3   4  5   6
+        # god this is ugly, but need to update to python 3.10 for match statment :((
+        if processedInput == 'mo':
+            processedInput = 0
+        elif processedInput == 'tu':
+            processedInput = 1
+        elif processedInput == 'we':
+            processedInput = 2
+        elif processedInput == 'th':
+            processedInput = 3
+        elif processedInput == 'fr':
+            processedInput = 4
+        elif processedInput == 'sa':
+            processedInput = 5
+        elif processedInput == 'su':
+            processedInput = 6
+        else:
+            processedInput = -1
+        
+        if processedInput == -1:
+            await ctx.channel.send(f'I dont recognise ``{arg}`` as a day of the week.')
+        else:
+            teams = get_teams()
+            teaminfo = get_days_scrimming_teams(teams, processedInput)
+            output = [ti[0] for ti in teaminfo]
+            await ctx.channel.send(f"The A51 teams that scrim on {arg} are: ``{', '.join(output)}``")
+
+@client.command(
+    # ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+	help=f"Updates the bots information on A51 teams. Can only be used by {', '.join(PROTECTED_COMMANDS_ALLOWED_ROLES)}",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="[!upd] Updates the bots information on A51 teams.",
+    aliases=['upd', 'refresh', 'reload']
+)
+async def update(ctx):
+    print(f'{ctx.author.name} sent command !update')
+    is_allowed = False
+    if ctx.channel.id in BOTCOMMANDS_ALLOWED_CHANNELS:
+        for role in PROTECTED_COMMANDS_ALLOWED_ROLES:
+            if role in [y.name.lower() for y in ctx.author.roles]:
+                is_allowed = True
+                await ctx.channel.send(f"Updating information. This may take a moment....")
+                main()
+                await ctx.channel.send(f"Bot records have been updated!")
+        if not is_allowed:
+            await ctx.channel.send(f"This command can only be used by people with the roles {', '.join(PROTECTED_COMMANDS_ALLOWED_ROLES)}")
+    
+
+@client.command(
+    # ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+	help=f"Sends the user the stream schedule for next week. Can only be used by {', '.join(PROTECTED_COMMANDS_ALLOWED_ROLES)}",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="[!gen] Sends the user the stream schedule for next week.",
+    aliases=['gen']
+)
+async def generate(ctx):
+    print(f'{ctx.author.name} sent command !generate')
+    is_allowed = False
+    if ctx.channel.id in BOTCOMMANDS_ALLOWED_CHANNELS:
+        for role in PROTECTED_COMMANDS_ALLOWED_ROLES:
+            if role in [y.name.lower() for y in ctx.author.roles]:
+                is_allowed = True
+                await ctx.channel.send(f"Aye Aye Captain!")
+                newSchedule = main()
+                await ctx.author.send(f"Hello there! I believe you requested the schedule for next week:")
+                await ctx.author.send(f"```{newSchedule}```")
+        if not is_allowed:
+            await ctx.channel.send(f"This command can only be used by people with the roles {', '.join(PROTECTED_COMMANDS_ALLOWED_ROLES)}")
+    
+
+client.run(token)
---
+++ main.py
@@ -0,0 +1,350 @@
+from gspread import service_account
+from gspread_formatting import get_user_entered_format
+from datetime import timedelta, date, datetime
+from json import dumps
+
+# ----- difine constants -----
+# Create the service account from the json private key, allowing use access to the teams spreadsheet
+SERVICE_ACCOUNT = service_account(filename='plasma-respect-323910-617fd6c81027.json')
+SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1n-W6oyEv4HehYN3gYcUVk7dOJQUP_Y70L7AN_Sp8Lgo/edit#gid=2043552790'
+SPREADSHEET = SERVICE_ACCOUNT.open_by_url(SPREADSHEET_URL)
+WORKSHEET = SPREADSHEET.get_worksheet(1)    # We only want to use the 2nd worksheet, 'Team Schedule'
+WORKSHEET_AVAILABLE_CELLFORMAT = get_user_entered_format(WORKSHEET,'C10')   # This cell is constant, formatted with the available color
+WORKSHEET_NOTAVAILABLE_CELLFORMAT = get_user_entered_format(WORKSHEET,'C11')# This cell is constant, formatted with the not available color
+
+
+def cellFormat_to_color(cellformatstring):
+    '''Small Global function used to convert gspread_formatting.CellFormat data to RGB color values. 
+    It only returns the background color of a cell. Behaviour undefined if the cell has no background color attribute.
+    
+    Parameters:
+        cellformatstring : cellFormat object as a string (retrieved useing get_user_entered_format(WORKSHEET,LABEL)). Must have backgroundcolor attribute as first attribute.
+    Return:
+        [float, float, float] : List of floats rounded to 2 dp, corresponding to the red, green and blue channels respectivly.
+    '''
+
+    # Use string comprehension to get the first attrib, almost always background color
+    backgroundcolor = cellformatstring.split(')')[0]
+    backgroundcolor = backgroundcolor.replace('backgroundColor=(', '').split(';')
+    # format from list of strings to floats (removes the color tags red=, green=, blue=)
+    if backgroundcolor[0] != 'None':
+        for i, v in enumerate(backgroundcolor):
+            backgroundcolor[i] = round(float(v.split('=')[1]), 2)
+        return backgroundcolor
+    return None
+
+# use the constant cells in the sheets key to deturmine what the format for avaliable and not available is
+AVAILABLE_COLOR = cellFormat_to_color(str(WORKSHEET_AVAILABLE_CELLFORMAT))
+NOTAVAILABLE_COLOR = cellFormat_to_color(str(WORKSHEET_NOTAVAILABLE_CELLFORMAT))
+# used to store the previous stream schedules. This would be the scraped discord message(s) as a string  
+PREVIOUS_SCHEDULES_TEXTFILE = 'D:\\Users\\geosp\\Documents\\Code\\PY\\Projects\\A51 Team Scheduler\\previousSchedules.txt'
+# used to store information about teams ad scheduleing. This is what the discord bot reads and relays to users
+TEAM_SCHEDULE_INFO_JSONFILE = 'D:\\Users\\geosp\\Documents\\Code\\PY\\Projects\\A51 Team Scheduler\\ScheduleInfo.json'
+JSON_DUMP_OBJECT = {  }
+
+
+# ---------------------- consts setup finished ----------------------  #
+'''
+Note:
+    1. All hardcoded ranges and cells (eg, WORKSHEET_AVAILABLE_CELLFORMAT, or thoes in get_teams()) are protected on the google sheet, meaning they cannot be changed.
+
+'''
+
+
+
+def is_cell_available(cell):
+    '''Given a cell in the format "A1", deturmine weather its formatted in the available or not available color.
+    Paramters:
+        cell : The cell to read, given as a string with the first letter being the column, and the proceeding number being the row. No spaces.
+    Return:
+        bool : True if the cell given has the same background color as the available keyed cell.
+     '''
+
+     # get the color of the cell the user input
+    cell_format_string = str(get_user_entered_format(WORKSHEET, cell))
+    cell_color = cellFormat_to_color(cell_format_string)    
+    # return True if the team is available
+    if cell_color == AVAILABLE_COLOR:
+        return True
+    if cell_color == NOTAVAILABLE_COLOR:
+        return False
+    # if the cell given is niether, return False with a message
+    print(f'Cell {cell} has color:\n\t{cell_color}\nwhich does not match iether:\n\tAVAILABLE: {AVAILABLE_COLOR}\n\tNOT AVAILABLE: {NOTAVAILABLE_COLOR}\n Assuming team in cell is not available.')
+    return False
+
+
+def get_teams():
+    '''Retrieve a list of teams available to be placed on the schedule from the google sheet.
+    Return:
+        A list of 'team' lists, where each 'team' has the format:
+        [team name, mon,  tue,  wed,  thur, fri,  sat,  sun,  score]
+        [string,    bool, bool, bool, bool, bool, bool, bool, int  ]
+        The booleans for days denote weather a team is scrimming that day. The score is always 0.
+    '''
+    # get all values in the spreadsheet, remove the first 12 rows so the list starts on the first team. 
+    # This will always be 12 (see Note 1. above) .
+    raw_team_list = WORKSHEET.get_all_values()
+    raw_team_list = raw_team_list[12:]
+    team_list = []
+
+    # loop through all rows that have team info, if the team name (row[7]) is blank, the end of the table ahs been reached 
+    for row in raw_team_list:
+        if row[6] == '':
+            break
+        team_list.append(row[6:14])
+        
+    # format scrim days as a bool (replace all present text with True, excluding team name. '' = False)
+    for index, value in enumerate(team_list):
+        for innerindex, innervalue in enumerate(value):
+            if innervalue == '':
+                team_list[index][innerindex] = False
+            elif innerindex > 0:    # for all values that re not the first ahve have text, set them to True
+                team_list[index][innerindex] = True
+            else:                   # change the first value (team name) to uppercase no spaces
+                team_list[index][innerindex] = team_list[index][innerindex].replace(' ', '').upper()
+        team_list[index].append(0)  # append score to the end of each team. Score set to 0 by default
+
+    # remove teams that are not currently on the schedule
+    team_list_culled = []
+    for team_index, team_value in enumerate(team_list):
+        row = 13 + team_index                   # To get the cel row number, offest of 13 is needed as the first team is on row 13 in sheets
+        teamCell = 'G' + str(row)               # All team names have the avaialbility formatted and are on col G
+        if(is_cell_available(teamCell)):        # if the current team is highlighted as available on sheets,
+            team_list_culled.append(team_value) # add to the culled team list
+
+    # print info and log in json file
+    #print(f'There are {len(team_list)} teams total. {len(team_list_culled)} teams are currently on the schedule:')
+    #print([t[0] for t in team_list_culled])
+    JSON_DUMP_OBJECT["Teams"] = [t[0] for t in team_list]
+    JSON_DUMP_OBJECT["Teams On Schedule"] = [t[0] for t in team_list_culled]
+
+    return team_list_culled    
+        
+
+def get_day_order(teamlist):
+    ''' Gets a list of days and the number of scrims on each day, ordered from least scrims to most.
+    Parameters:
+        teamlist : list of teams to evaluate. Scrim days should be index 1-7 and be bool values.
+    Return:
+        List of lists, where inner list has the following format, sorted by number of scrims low to high:
+        [ int,       int ]
+        [ day index, number of scrims ]
+        day index corrisponds to a day, where Where mon = 0, tue = 1, ..., sun = 6
+    '''
+
+    dayList = [] 
+    for i in range(7):                      # for each day
+        scrimCounter = 0
+        for team in teamlist:               # count the number fo True values in the team list
+            if(team[i+1] == True):
+                scrimCounter += 1
+        dayList.append([i, scrimCounter])   # append [day index, number of scrims] to return list
+    
+    return sorted(dayList, key=lambda x: x[1])# sort low to high by number of scrims
+
+
+def get_previous_schedule():
+    ''' Gets the string of previous shedule(s) by reading the text file. 
+    Return:
+        list of eah day, with spaces removed and in all caps.
+    '''
+    # open file, read all, format. The split val separates each day
+    with open(PREVIOUS_SCHEDULES_TEXTFILE) as f:
+        ps = f.readlines()
+    return ''.join(ps).replace(' ', '').upper().split('-----------------------------------------')
+
+
+def calculate_team_scores(teams):
+    ''' The main scheduler. Scores each team from low (not likey to be streamed) to high (likely) and adds it to the score value
+        The score is arbitary, and is based on the number of days since a team has been streamed. The number of days the 
+        algoritm checks (lookback) is based on how many days are stored in PREVIOUS_SCHEDULES_TEXTFILE, once this limit is reached,
+        all remaining unscored teams gain the same, highest score possible.
+
+        Score is calculated by itterating over the previous schedule, starting from the most recent day. A count of each day its 
+        looked back is kept (lookback), and this is used for the score. If a team is found on a day, that team is given the current 
+        lookback as its score.  
+    
+        Parameters:
+            teams : the list of teams (only the name and score are used)
+        Return:
+            None (teams list is edited directly)
+    '''
+
+    # get previous schedule and lookback
+    previouseStreams = get_previous_schedule()
+    lookback = len(previouseStreams)
+
+    # calculate the lower bracket of scores, for teams that were previousely on the stream schedule
+    for i in range(lookback):
+        for team_index, team_data in enumerate(teams):
+            if team_data[0] in previouseStreams[(lookback - 1) - i]:
+                teams[team_index][8] = i
+    
+    # for teams that have not been streamed in a while, give them the highest score
+    # calculate max
+    highestScore = max( [team[8] for team in teams] )
+    for team_index, team_data in enumerate(teams):
+        if(team_data[8] == 0):  # if score has not been changed, set to max
+            teams[team_index][8] = highestScore + 1
+
+    # store team name and its score in the json file
+    teamScore = []
+    for team in teams:
+        teamScore.append([team[0], team[8]])    # add team name and scores to list
+    JSON_DUMP_OBJECT["TeamScore"] = teamScore
+    JSON_DUMP_OBJECT["Highest Priority"] = highestScore + 1
+
+
+def get_days_scrimming_teams(teams, dayIndex):
+    ''' Get all the teams that have a scrim on a specific day
+    Parameters:
+        teams : the list of teams
+        dayIndex : The index of the day to search (0=Mon, 1=Tue, ..., 6=Sun)
+    Return:
+        List of all teams that are scriming on a given day, sorted by their score highest to lowest 
+    '''
+
+    # 0 = Mon, 1 = Tue, ..., 6 = Sun
+    teamsAvailable = []
+    for teamData in teams:
+        if(teamData[dayIndex + 1] == True): # +1 as the first value in the team list is the team name
+           teamsAvailable.append(teamData)
+
+    return sorted(teamsAvailable, key=lambda x: x[8], reverse=True)
+
+
+def next_weekday(d, weekday):
+    ''' KNOWN ISSUES: date not always correct when its into next month
+        This was taken from stack overflow, beacuse who in their right mind wants to deal with dates (not me lol)
+        Should, given a day, return the date of the net occurance of that day.
+    Parameters:
+        d : the date to start the search (often the current date)
+        weekday : the weekday to search for
+    Return:
+        a date object of the next occurance of weekday from date d
+    '''
+
+    days_ahead = weekday - d.weekday()
+    if days_ahead <= 0: # Target day already happened this week
+        days_ahead += 7
+    return d + timedelta(days_ahead)
+
+
+def get_schedule(teams):
+    ''' Places teams into each day of the week (starting with the day that has the least teams scrimming) based on their score
+    Substitue teams are also calculated, but a bit differently. Firstly, once the main teams have been selected and removed from 
+    the pool of available teams, the next team with the highest score is chosen to be a sub. Then for all days that chosen team 
+    scrim, they are placed as a sub. Then they are removed from the pool, and the process repeats, ensuring that the minimal amount
+    of teams are chosen as subs.
+
+    Paramenters:
+        teams : the list of teams (name and score are read)
+    Return:
+        A list ordered for each day of the week (0=Mon, ect..) that contains the names of 1 to 3 teams. The team in index 0 is the
+        main team. Any following teams are substitute teams, up to 2 subs. Sub teams are not guaranteed. 
+    '''
+
+    # get a formatted list of scrim days [day, number of active teams] sorted by least to most popular day 
+    dayOrder = get_day_order(teams)
+    final_order = ['', '', '', '', '', '', '']  # placeholders used to ensure teams are kept in the right days
+    remainingTeams = teams.copy()   # used as the pool of teams to choose from. Teams chosen will be removed, so coppy list.
+
+    for dayIndex, _ in dayOrder:    # list is already sorted, so the 2nd 'number of teams scrimming' value can be ignored
+        available_teams = get_days_scrimming_teams(remainingTeams, dayIndex)
+        #print(f'for day {dayIndex} avTeams: {[t[0] for t in available_teams]}')
+        if available_teams[0]:  # if there exists teams to place on the schedule, place the next highest scoring team
+            final_order.insert(dayIndex, [available_teams[0][0]]) # only insert the teamname (no other data is needed)
+            del final_order[dayIndex + 1]
+            remainingTeams.remove(available_teams[0])   # remove chosen team from the pool
+        else:   # failsafe, if for some reason there are no available teams that scrim on a day, set team name to 'no stream'
+            final_order.insert(dayIndex, [' No Stream'])
+
+    # calculate the subs.
+    # if a team is a sub, then they should be a sub for all days they scrim 
+    for j in range(len(remainingTeams)):    # for all available teams remaining
+        for i, data in enumerate(remainingTeams[j]):
+            if data == True and type(data) == bool: # for each day that team scrims
+                if (len(final_order[i-1]) <=2):     # if there is room on the final list to add them as a sub, add them
+                    final_order[i-1].append(remainingTeams[j][0])
+    JSON_DUMP_OBJECT["Future Schedule"] = final_order
+    return final_order
+   
+
+def format_schedule(teamlist):
+    ''' Formats the final order of the teams into a discord message, and returns the final string
+    Paramters:
+        teamlist : List (ordered for each day) of lists (team names in order of priority to add for that day)
+    Return:
+        Formatted string of schedule with sub teams denoted
+    '''
+
+    # get next weeks (from current date) monday and sunday date 
+    d = date.today()
+    next_monday = next_weekday(d, 0) # 0 = Monday, 1=Tuesday, 2=Wednesday...
+    next_sunday = next_weekday(d, 6)
+
+    discord_message_format = f"""
+    Week {next_monday.day}/{next_monday.month} - {next_sunday.day}/{next_sunday.month}
+
+    Monday scrim 
+    @{teamlist[0][0]} (sub: {teamlist[0][1]})
+    -----------------------------------------
+    Tuesday scrim 
+    @{teamlist[1][0]} (sub: {teamlist[1][1]})
+    -----------------------------------------
+    Wednesday scrim
+    @{teamlist[2][0]} (sub: {teamlist[2][1]})
+    -----------------------------------------
+    Thursday scrim 
+    @{teamlist[3][0]} (sub: {teamlist[3][1]})
+    -----------------------------------------
+    Friday scrim 
+    @{teamlist[4][0]} (sub: {teamlist[4][1]})
+    -----------------------------------------
+    Saturday scrim 
+    @{teamlist[5][0]} (sub: {teamlist[5][1]})
+    -----------------------------------------
+    Sunday scrim 
+    @{teamlist[6][0]} (sub: {teamlist[6][1]})
+
+    *Subject to broadcaster availability"""  
+    return discord_message_format
+
+
+
+def main():
+    '''  produces a stream schedule '''
+
+    # get a formatted list of teams [name, bool scrim days...., score]
+    print('--- schedule generation ---\n\tRetrieving teams from Google Sheets...')
+    teams = get_teams()
+    # print(str(teams).replace('],', '],\n'))
+
+    # calculate team scores
+    print('\tCalculating team priority scores...')
+    calculate_team_scores(teams)
+
+    # construct schedule
+    print('\tConstructing the schedule....')
+    order = get_schedule(teams)
+
+    # write data to json file
+    JSON_DUMP_OBJECT['Date Compiled'] = str(datetime.now())
+    with open(TEAM_SCHEDULE_INFO_JSONFILE, "w") as f:
+        f.write(dumps(JSON_DUMP_OBJECT))
+
+    print('\tFinished!\n--- schedule generation ---')
+
+    # output the formated schedule
+    return format_schedule(order)
+
+if __name__ == '__main__':
+    print(main())
+    
+    
+
+
+
+
+
+
