e21546470f4a47b0b1b5c63d8009bc18e33bbf32
GeorgeSpiller <57860253+GeorgeSpiller@users.noreply.github.com>
2022-08-16 10:44:40 +0100

    Started extracting utility funuctions to separate file

--- DiscordUtils.py
+++ DiscordUtils.py
@@ -8,7 +8,7 @@ from Schedule import get_teams, get_days_scrimming_teams, main
 from Constants import *
 
 
-def replace_roleid_with_rolename(message, guild):
+def dutil_replace_roleid_with_rolename(message, guild):
     '''replaces any instances of role ID tags with the role corrisponding role's name.
         Also removes any roles that are between ~~strikethrough~~ tags'''
     # fist, get a list of all the ID's in the message
@@ -32,7 +32,7 @@ def replace_roleid_with_rolename(message, guild):
     return ret
 
 
-async def count_total_casts(userID, client):
+async def dutil_count_total_casts(userID, client):
     counter = 0
     async for message in client.get_channel(CASTERSIGNUP_CHANNEL_ID).history(limit=None):
         if str(userID) in message.content:
@@ -40,7 +40,7 @@ async def count_total_casts(userID, client):
     return counter
 
 
-def build_embed(tupledata):
+def dutil_build_embed(tupledata):
     cotm_prod, full_month_name, prefix, verb = tupledata
 
     # embed the information
@@ -68,7 +68,7 @@ def build_embed(tupledata):
     return embed
 
 
-def build_embed_nomonth(tupledata):
+def dutil_build_embed_nomonth(tupledata):
     user, score = tupledata
 
     # embed the information
@@ -92,9 +92,93 @@ def build_embed_nomonth(tupledata):
     return embed
 
 
+async def dutil_write_previous_schedule(guild, textChannel):
+    messageList = []
+    async for message in textChannel.history(limit=PREVIOUS_SCHEDULE_LOOKBACK):
+        messageList.append(message.content)
+    messageList.reverse()
+    PREVIOUS_SCHEDULE_STRING = '-----------------------------------------'.join(messageList)
+    PREVIOUS_SCHEDULE_STRING = dutil_replace_roleid_with_rolename(PREVIOUS_SCHEDULE_STRING, guild)
+    with open(PREVIOUS_SCHEDULES_TEXTFILE, 'w') as f:
+        f.write(PREVIOUS_SCHEDULE_STRING)
 
 
+def dutl_dayString_to_dayumber(inp):
+    #   m, tu, w, th, f, sa, su,
+    #   0  1   2  3   4  5   6
+    # god this is ugly, but need to update to python 3.10 for match statment :((
+    if inp == 'mo':
+        inp = 0
+    elif inp == 'tu':
+        inp = 1
+    elif inp == 'we':
+        inp = 2
+    elif inp == 'th':
+        inp = 3
+    elif inp == 'fr':
+        inp = 4
+    elif inp == 'sa':
+        inp = 5
+    elif inp == 'su':
+        inp = 6
+    else:
+        inp = -1
+    return inp
 
 
+async def dutil_cotm(client, arg, ctx):
+    currentMonth = datetime.now().month
+    lastMonth = currentMonth - 1 if (currentMonth - 1) != 0 else 12
+    producers = []
+    caster_pbp = []
+    caster_col = []
+    caster_signup_channel = client.get_channel(CASTERSIGNUP_CHANNEL_ID)
+    async for message in caster_signup_channel.history(limit=45*2):   # 45 is the maximum amount of messages a month the bot that posts in this channel could post
+        if message.created_at.month == lastMonth:
+            if '@' in message.content:
+                producer_search = ' '.join(re.findall('Production\/Observer\(🎥\): (<@\d{18}> ?)*', message.content))
+                caster_pbp_search = ' '.join(re.findall('Play-By-Play\(🎙\): (<@\d{18}> ?)*', message.content))
+                caster_col_search = ' '.join(re.findall('Colour\(🔬\): (<@\d{18}> ?)*', message.content))
 
+                if producer_search:
+                    producers.append(producer_search.replace('<', '').replace('>', '').replace('@', ''))
+                
+                if caster_pbp_search:
+                    caster_pbp.append(caster_pbp_search.replace('<', '').replace('>', '').replace('@', ''))
 
+                if caster_col_search:
+                    caster_col.append(caster_col_search.replace('<', '').replace('>', '').replace('@', ''))
+
+    # count the occurance of each name, based on which role was inout as an arg
+    datetime_object = datetime.strptime(str(lastMonth), "%m")
+    full_month_name = datetime_object.strftime("%B")
+
+    broadcasterDict = {}
+    winnerData = []
+    embed = None
+    winnerUser = None
+
+    if arg == "prod":
+        for entry in producers:
+            broadcasterDict[entry] = producers.count(entry)
+        winnerUser = await client.fetch_user(int(max(broadcasterDict, key=broadcasterDict.get)))
+        winnerData = [winnerUser, broadcasterDict[str(winnerUser.id)], await dutil_count_total_casts(winnerUser.id, client)]
+        embed = dutil_build_embed( (winnerData, full_month_name, "Producer", "Produced") )
+        await ctx.send(embed=embed)
+
+    elif arg == "pbp": 
+        for entry in caster_pbp:
+            broadcasterDict[entry] = caster_pbp.count(entry)
+        winnerUser = await client.fetch_user(int(max(broadcasterDict, key=broadcasterDict.get)))
+        winnerData = [winnerUser, broadcasterDict[str(winnerUser.id)], await dutil_count_total_casts(winnerUser.id, client)]
+        embed = dutil_build_embed( (winnerData, full_month_name, "Play-by-Play Caster", "Casted") )
+        await ctx.send(embed=embed)
+    
+    elif arg == "col":
+        for entry in caster_col:
+            broadcasterDict[entry] = caster_col.count(entry)
+        winnerUser = await client.fetch_user(int(max(broadcasterDict, key=broadcasterDict.get)))
+        winnerData = [winnerUser, broadcasterDict[str(winnerUser.id)], await dutil_count_total_casts(winnerUser.id, client)]
+        embed = dutil_build_embed( (winnerData, full_month_name, "Color Caster", "Casted") )
+        await ctx.send(embed=embed)
+                    
